f77e04c8c7af1a370f7eff47c075206bf20aba96
Benson Diep <bdiep45@gmail.com>
2023-11-06 16:49:06 -0800

    Created File

---
+++ ButtonElement.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class ButtonElement extends JButton implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+	}
+	
+}
---
+++ Palette.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Palette {
+	private static Color text;
+	private static Color background;
+	private static Color element;
+
+	public static Color getText(){
+		return text;
+	}
+
+	public static Color getBackground(){
+		return background;
+	}
+
+	public static Color getElement(){
+		return element;
+	}
+}
